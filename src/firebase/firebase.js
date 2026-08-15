78b920869bb716351e35b6cc856a13e3e904bcee
Anandhu <anandhurcb@gmail.com>
2026-08-15 19:33:52 +0530

    Update Firebase configuration

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYn3BMT4_7FJJ5NlckJDn7S_bV-IV1Z1E",
-  authDomain: "eduhome-cec54.firebaseapp.com",
-  projectId: "eduhome-cec54",
-  storageBucket: "eduhome-cec54.firebasestorage.app",
-  messagingSenderId: "627428198711",
-  appId: "1:627428198711:web:5c8aa7a3d2bdebb38eb2b2",
+  apiKey: "AIzaSyB6Klq373uwOTZoa6RVWfWI4aXLOP0xQYk",
+  authDomain: "eduhome-d8b01.firebaseapp.com",
+  projectId: "eduhome-d8b01",
+  storageBucket: "eduhome-d8b01.firebasestorage.app",
+  messagingSenderId: "610177244432",
+  appId: "1:610177244432:web:200224863a7e2575571995",
   measurementId: "G-SCBM6PCWPB"
 };
 
